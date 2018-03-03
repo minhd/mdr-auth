981dfdab44c01c2b59e970863648710307691f8f
Minh Duc Nguyen <dekarvn@gmail.com>
2018-03-03 16:15:27 +1100

    Added Repository API

---
+++ app/Http/Controllers/API/Repository/RepositoryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MinhD\Http\Controllers\API\Repository;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use MinhD\Http\Controllers\Controller;
+
+class RepositoryController extends Controller
+{
+    public function index()
+    {
+        return [
+            'url' => URL::to('/'),
+            'api_url' => URL::to('/api'),
+            'version' => '0.1a'
+        ];
+    }
+}
